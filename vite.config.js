b164d35138f42fab0552259c4bf9f50f9c160f9a
chilichen0118 <chilichen0118@gmail.com>
2023-08-13 18:02:36 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/vite_test/',
+  base: '/week4_task2023/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
